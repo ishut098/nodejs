392b295b90ff3a2799c34bcabe1ba855bb7ccd20
rishu <abc@example.com>
2019-01-11 17:36:09 +0530

    Second commit

--- ishut098/07-express/03-products-server/index.js
+++ ishut098/07-express/03-products-server/index.js
@@ -5,17 +5,19 @@ const reviewRouter=require('./router/reviews.js')
 const app=express();
 const productRouter=require('./router/products.js');
 
+app.use(express.json()); //enables to form json data
+app.use(express.urlencoded({extended:false}));// enables to pass form data
 
 app.use(indexRouter); //Associating the app with the router.
-app.use(productRouter);
+app.use('/products',productRouter);
 app.use(reviewRouter);
 
-app.listen(8091,function(error){
+app.listen(8092,function(error){
     if(error){
         console.log('error starting server');
         
         return;
     }
-    console.log('Check http://localhost:8091');
+    console.log('Check http://localhost:8092');
 });
 
--- ishut098/07-express/03-products-server/router/products.js
+++ ishut098/07-express/03-products-server/router/products.js
@@ -3,12 +3,74 @@ const router=express.Router();
 const data=require('../data/seed.json');
 
 
-router.get('/products',function(req,res){
+router.get('/',function(req,res){
     res.json(data.products);
 }); //To get all the products in JSON format
 
 
-router.get('/products/:productId',function(req,res){
+router.post('/',function(req,res){
+    const product = req.body;
+    if(!req.body){
+        //false,undefined,null,empty
+        res.json({
+            message:'Product Details Empty'
+        });
+        return;
+    }
+    //for incomplete details
+    //test for name price code are compulsory
+    //release date to be set to current date if not given
+
+    if(!product.name || !product.price || !product.code){
+        res.json({
+            message:'Product name, price and code number are missing'
+        });
+        return;
+    }
+
+    if(!product.releaseDate){
+        product.releaseDate=(new Date()).toString();
+    }
+    //generating product ID
+    
+    product.id = data.products.length+1; //bad logic but DB will do the work
+    
+    data.products.push(product);
+    res.status(201).json(product);
+
+});
+
+router.put('/:productId',function(req,res){
+    const productId=parseInt(req.params.productId);
+    if(isNaN(productId)){
+        res.status( 400 ).json({
+            message: 'Product ID is not a number'
+        });
+        return;
+    }
+    const product=req.body;
+    if(!product){
+        res.json({
+            message:'No product details- request body is empty'
+        });
+        return;
+    }
+    let matchingIndex;
+    const matchingProduct= data.products.find(function(product,index){
+        if(product.id===productId){
+            matchingIndex=index;
+        }
+        return product.id===productId;
+    });
+
+    data.products[matchingIndex]={...matchingProduct,...product};
+    res.json(data.products);
+
+});
+
+
+
+router.get('/:productId',function(req,res){
     const productId=parseInt(req.params.productId);
     if(isNaN(productId)){
         res.status( 400 ).json({
